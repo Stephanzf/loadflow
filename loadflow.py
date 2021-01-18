5c60479680c4a87ca961a5cc6ca62bf5778d7201
Stephanzf <stephan.ao@ualberta.net>
2021-01-19 01:48:44 +0800

    add loadflow.py as main in Python

---
+++ loadflow.py
@@ -0,0 +1,19 @@
+import sys
+from sys import stderr
+
+
+"""
+A load flow program
+"""
+
+def fdlf():    
+    str = "Calculate load flow for a network..."
+    print(str)
+    
+    
+
+
+if __name__ == '__main__':
+    fdlf()
+    
+
